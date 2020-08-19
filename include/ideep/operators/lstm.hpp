33fb663c53381c2806d991648dad76dfb6bb6e10
mingfeima <mingfei.ma@intel.com>
2020-08-19 08:32:14 +0800

    forward inference lstm

--- include/ideep/operators/lstm.hpp
+++ include/ideep/operators/lstm.hpp
@@ -2,9 +2,26 @@
 #define IDEEP_OPERATORS_LSTM_HPP
 
 namespace ideep {
-// test
-struct lstm_forward : public dnnl::lstm_forward {
-  static void compute() {
+
+struct lstm_forward_inference : public dnnl::lstm_forward {
+
+  using super = dnnl::lstm_forward;
+
+  static void compute(const tensor& src_layer,
+                      const tensor& src_iter,
+                      const tensor& src_iter_c,
+                      const tensor& weights_layer,
+                      const tensor& weights_iter,
+                      const tensor& bias,
+                      tensor& dst_layer,
+                      tensor& dst_iter,
+                      tensor& dst_iter_c,
+                      const bool reverse = false,
+                      const engine& aengine = engine::cpu_engine()) {
+    auto aprop = prop_kind::forward_inference;
+    auto direction = reverse ? rnn_direction::unidirectional_right2left
+                             : rnn_direction::unidirectional_left2right;
+
   }
 };
 
