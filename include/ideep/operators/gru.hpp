c07aa04842d430c3f1d2e548e9b327737a5d1941
mingfeima <mingfei.ma@intel.com>
2020-08-25 12:29:58 +0800

    add one hidden rnn

--- include/ideep/operators/gru.hpp
+++ include/ideep/operators/gru.hpp
@@ -3,12 +3,53 @@
 
 namespace ideep {
 
-struct gru_forward : public dnnl::gru_forward {
-  static void compute() {
+struct gru_forward_inference : public dnnl::lbr_gru_forward {
+
+  using super = dnnl::lbr_gru_forward ;
+
+  static void compute(const tensor& src_layer,
+                      const tensor& src_iter,
+                      const tensor& weights_layer,
+                      const tensor& weights_iter,
+                      const tensor& bias,
+                      tensor& dst_layer,
+                      tensor& dst_iter,
+                      const bool reverse = false,
+                      const engine& aengine = engine::cpu_engine()) {
+    auto aprop = prop_kind::forward_inference;
+    auto direction = reverse ? rnn_direction::unidirectional_right2left
+                             : rnn_direction::unidirectional_left2right;
+
+    auto src_layer_desc = src_layer.get_desc();
+    auto src_iter_desc = src_iter.get_desc();
+    // use any format for weights
+    auto weights_layer_desc = weights_layer.get_desc().to_format_any();
+    auto weights_iter_desc = weights_iter.get_desc().to_format_any();
+    auto bias_desc = bias.get_desc();
+    auto dst_layer_desc = dst_layer.get_desc();
+    auto dst_iter_desc = dst_iter.get_desc();
+
+    auto pd = primitive_desc(
+        {aprop, direction, src_layer_desc, src_iter_desc,
+         weights_layer_desc, weights_iter_desc, bias_desc,
+         dst_layer_desc, dst_iter_desc},
+        aengine);
+
+    auto expected_weights_layer = weights_layer.reorder_if_differ_in(pd.weights_desc());
+    auto expected_weights_iter = weights_iter.reorder_if_differ_in(pd.weights_iter_desc());
+
+    super(pd).execute(stream::default_stream(),
+                      {{DNNL_ARG_SRC_LAYER, src_layer},
+                       {DNNL_ARG_SRC_ITER, src_iter},
+                       {DNNL_ARG_WEIGHTS_LAYER, expected_weights_layer},
+                       {DNNL_ARG_WEIGHTS_ITER, expected_weights_iter},
+                       {DNNL_ARG_BIAS, bias},
+                       {DNNL_ARG_DST_LAYER, dst_layer},
+                       {DNNL_ARG_DST_ITER, dst_iter}});
   }
 };
 
-struct gru_backward : public dnnl::gru_backward {
+struct gru_backward : public dnnl::lbr_gru_backward {
   static void compute() {
   }
 };
--- include/ideep/operators/vanilla_rnn.hpp
+++ include/ideep/operators/vanilla_rnn.hpp
@@ -3,13 +3,50 @@
 
 namespace ideep {
 
-struct rnn_forward : public dnnl::vanilla_rnn_forward {
-  static void compute(const tensor& src_layer, const tensor& src_iter,
-      const tensor& weights_layer, const tensor& weights_iter, const tensor& bias,
-      const dims& dst_layer_dims, tensor& dst_layer,
-      const dims& dst_iter_dims, tensor& dst_iter,
-      tensor& workspace, rnn_kind akind, dnnl_rnn_direction_t direction,
-      prop_kind aprop_kind = prop_kind::forward_training) {
+struct rnn_forward_inference : public dnnl::vanilla_rnn_forward {
+
+  using super = dnnl::vanilla_rnn_forward;
+
+  static void compute(const tensor& src_layer,
+                      const tensor& src_iter,
+                      const tensor& weights_layer,
+                      const tensor& weights_iter,
+                      const tensor& bias,
+                      tensor& dst_layer,
+                      tensor& dst_iter,
+                      const rnn_kind akind,
+                      const bool reverse = false,
+                      const engine& aengine = engine::cpu_engine()) {
+    auto aprop = prop_kind::forward_inference;
+    auto activation = utils::rnn_kind_to_activation(akind);
+    auto direction = reverse ? rnn_direction::unidirectional_right2left
+                             : rnn_direction::unidirectional_left2right;
+    auto src_layer_desc = src_layer.get_desc();
+    auto src_iter_desc = src_iter.get_desc();
+    // use any format for weights
+    auto weights_layer_desc = weights_layer.get_desc().to_format_any();
+    auto weights_iter_desc = weights_iter.get_desc().to_format_any();
+    auto bias_desc = bias.get_desc();
+    auto dst_layer_desc = dst_layer.get_desc();
+    auto dst_iter_desc = dst_iter.get_desc();
+
+    auto pd = primitive_desc(
+        {aprop, activation, direction, src_layer_desc, src_iter_desc,
+         weights_layer_desc, weights_iter_desc, bias_desc,
+         dst_layer_desc, dst_iter_desc},
+        aengine);
+
+    auto expected_weights_layer = weights_layer.reorder_if_differ_in(pd.weights_desc());
+    auto expected_weights_iter = weights_iter.reorder_if_differ_in(pd.weights_iter_desc());
+
+    super(pd).execute(stream::default_stream(),
+                      {{DNNL_ARG_SRC_LAYER, src_layer},
+                       {DNNL_ARG_SRC_ITER, src_iter},
+                       {DNNL_ARG_WEIGHTS_LAYER, expected_weights_layer},
+                       {DNNL_ARG_WEIGHTS_ITER, expected_weights_iter},
+                       {DNNL_ARG_BIAS, bias},
+                       {DNNL_ARG_DST_LAYER, dst_layer},
+                       {DNNL_ARG_DST_ITER, dst_iter}});
   }
 };
 
